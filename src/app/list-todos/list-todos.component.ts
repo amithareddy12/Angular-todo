4fcc9ac7fcaa407bb5b943b0bb094b634fb17e84
amithareddy12 <20r11a1229@gcet.edu.in>
2022-09-01 09:43:05 -0700

    first commit

---
+++ src/app/list-todos/list-todos.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-todos',
+  templateUrl: './list-todos.component.html',
+  styleUrls: ['./list-todos.component.css']
+})
+export class ListTodosComponent implements OnInit {
+  todos = [
+    new Todo(1, 'Master Angular', true, new Date() ),
+    new Todo(2, 'Master Angular Material', true, new Date() ),
+    new Todo(3, 'Master Angular MDB', true, new Date() )
+  ]  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+
+
+
+export class Todo {
+  constructor(
+      public id: number,
+      public description: string,
+      public done: boolean,
+      public targetDate: Date
+  ){}
+  }
